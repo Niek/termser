7143460761d92f620e81e1b7f2f3bcea92755e91
Niek van der Maas <mail@niekvandermaas.nl>
2022-04-20 14:53:46 +0200

    Clean-up

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 // Imports
 const { chromium } = require('playwright')
-const term = require('terminal-kit').terminal
+const { terminal } = require('terminal-kit')
 const fetch = require('cross-fetch')
-const adblocker = require('@cliqz/adblocker-playwright').PlaywrightBlocker
+const { PlaywrightBlocker } = require('@cliqz/adblocker-playwright')
 
 // Define command line flags
 const argv = require('yargs')
@@ -41,21 +41,21 @@ const argv = require('yargs')
   console.log(`Screen width: ${argv.width}`)
   console.log(`Block ads: ${argv.adblock}`)
 
-  const height = parseInt((argv.width / term.width) * term.height * 2.4)
+  const height = parseInt((argv.width / terminal.width) * terminal.height * 2.4)
 
   const browser = await chromium.launch()
   const page = await browser.newPage({ viewport: { width: argv.width, height } })
 
-  term.clear()
-  term.hideCursor(true)
+  terminal.clear()
+  terminal.hideCursor(true)
 
   // Start streaming frames
   const client = await page.context().newCDPSession(page)
   await client.send('Page.startScreencast', { format: 'jpeg', quality: 80 })
   client.on('Page.screencastFrame', async (frame) => {
     if (process.env.TERM_PROGRAM !== 'iTerm.app') {
-      await term.clear()
-      await term.drawImage(`data:image/jpeg;base64,${frame.data}`, { shrink: { width: term.width, height: term.height } })
+      await terminal.clear()
+      await terminal.drawImage(`data:image/jpeg;base64,${frame.data}`, { shrink: { width: terminal.width * 2, height: terminal.height * 2 } })
     } else {
       console.log(ansiEscapes.image(Buffer.from(frame.data, 'base64'), { height: '100%' }))
     }
@@ -63,18 +63,17 @@ const argv = require('yargs')
   })
 
   if (argv.adblock) {
-    adblocker.fromPrebuiltAdsAndTracking(fetch).then((blocker) => {
-      blocker.enableBlockingInPage(page)
-    })
+    const blocker = await PlaywrightBlocker.fromPrebuiltAdsAndTracking(fetch)
+    blocker.enableBlockingInPage(page)
   }
 
   // Handle keys
-  term.on('key', async (name, matches, data) => {
+  terminal.on('key', async (name, matches, data) => {
     // See: https://playwright.dev/docs/api/class-keyboard#keyboard-press
     switch (name) {
       case 'CTRL_C':
-        term.grabInput(false)
-        term.hideCursor(false)
+        terminal.grabInput(false)
+        terminal.hideCursor(false)
         await browser.close()
         process.exit()
       case 'ESCAPE':
@@ -95,20 +94,20 @@ const argv = require('yargs')
   })
 
   // Handle mouse events
-  term.on('mouse', async (name, data) => {
+  terminal.on('mouse', async (name, data) => {
     if (name === 'MOUSE_LEFT_BUTTON_RELEASED') {
-      const x = argv.width / term.width * data.x
-      const y = height / term.height * data.y
+      const x = argv.width / terminal.width * data.x
+      const y = height / terminal.height * data.y
       await page.mouse.click(x, y)
     }
   })
-  term.grabInput({ mouse: 'button' })
+  terminal.grabInput({ mouse: 'button' })
 
   // Update terminal title
   page.on('framenavigated', async (frame) => {
     try {
       const title = await frame.title()
-      term.windowTitle(title)
+      terminal.windowTitle(title)
     } catch (e) { }
   })
 
